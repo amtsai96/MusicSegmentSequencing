67e9657c753e14c9137eaba3100279969bd652d3
Amanda Tsai <dingyupei24@gmail.com>
2019-09-08 03:00:06 +0800

    Plotting with Visdom

--- simple_tripletnet.py
+++ simple_tripletnet.py
@@ -2,6 +2,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class TripletNet(nn.Module):
     def __init__(self, embedding_net):
         super(TripletNet, self).__init__()
@@ -18,22 +19,20 @@ class TripletNet(nn.Module):
     def get_embedding(self, x):
         return self.embedding_net(x)
 
+
 class EmbeddingNet(nn.Module):
     def __init__(self):
         super(EmbeddingNet, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=3)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=3)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 100)
+        self.fc1 = nn.Linear(200, 100)
         self.fc2 = nn.Linear(100, 50)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
+        x = x.view(-1, 200)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         return self.fc2(x)
-
-
-
