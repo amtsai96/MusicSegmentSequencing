c3bd939358f0c14e78ed61c8e41ec5e5b1bbee54
amtsai96 <dingyupei24@gmail.com>
2019-09-08 21:34:13 +0800

    Edit train.py

--- train.py
+++ train.py
@@ -65,7 +65,6 @@ def main():
     base_path = './'  # './music_segments'
     # '/Users/amandatsai/git_folders/JazzMusicMashup/music_segments/'
     # '/Users/amandatsai/FILES/NCKU/Academia_Sinica/test/MASHUP/music_segments/'
-    '''
     train_loader = DataLoader(
         TripletAudioLoader(base_path, train=True,
                            transform=transforms.Compose([
@@ -94,7 +93,7 @@ def main():
             transforms.Normalize((0.1307,), (0.3081,))
         ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
-    
+    '''
 
     model = EmbeddingNet()
     tnet = TripletNet(model)
